51c0472d64898c4f13f633036f2abd561010fc45
rejawanul <rejhaqtonmoy@gmail.com>
2022-12-10 21:46:08 +0600

    Main extra file

--- hello.py
+++ hello.py
@@ -1,1 +1,7 @@
-print("sanbvd")
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+chrome_options = Options() 
+chrome_options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=chrome_options)
+driver.get('https://www.google.com')
