1bcfaad6e001dfac88b2ac5bdc4a4a6014916704
rejawanul <rejhaqtonmoy@gmail.com>
2022-12-10 21:53:30 +0600

    Main

--- hello.py
+++ hello.py
@@ -1,7 +1,11 @@
+#Importing packages from SELENIUM, we will work with "Chrome Browser"
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 chrome_options = Options() 
 chrome_options.add_experimental_option("detach", True)
+
+#Open Chrome Browser
 
 driver = webdriver.Chrome(options=chrome_options)
 driver.get('https://www.google.com')
