0ff41b7fbdb4f34acab3064ff2e3ad63968ada62
Md. Rejawanul Haque Tonmoy <91355015+rejawanul@users.noreply.github.com>
2022-12-25 06:32:10 +0600

    Update
    new part

--- hello.py
+++ hello.py
@@ -1,18 +1,4 @@
-'''#Importing packages from SELENIUM, we will work with "Chrome Browser"
-
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-chrome_options = Options() 
-chrome_options.add_experimental_option("detach", True)
-
-#Open Chrome Browser
-
-driver = webdriver.Chrome(options=chrome_options)
-driver.get('https://www.google.com')
-lin2 test
-sdvsdv
-rfujdofliu
-'''
+#Importing packages from SELENIUM, we will work with "Chrome Browser"
 
 import selenium
 from selenium import webdriver
